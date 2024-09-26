df4b19b3d0a940031ca4a3f8ba8545189d6a1a0d
suneshkk <suneshkk232@gmail.com>
2024-09-26 10:25:08 +0530

    commit

--- Backent/index.js
+++ Backent/index.js
@@ -6,33 +6,40 @@ import { errorHandle } from './util/errorHandle.js';
 import cookieParser from 'cookie-parser';
 import { apiRouter } from './routes/index.js';
 
-
-dotenv.config();
-connectDB();
 const app = express();
-app.use(express.json());
 const port = process.env.PORT || 5000;
+app.use(express.json());
 app.use(cookieParser());
-
 
 // cors controler setup
 const corsSetup = {
     origin: 'http://localhost:5000', // Allow all origins; you can specify specific origins instead
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTION'], // Allowed methods
     allowedHeaders: ['Content-Type', 'Authorization'],
 }
 app.use(cors(corsSetup));
 
+// app.get('/', (req, res) => {
+//     res.send({ message: 'CORS is working!' });
+// });
+
+dotenv.config();
+connectDB();
+
 // rout setup
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send({ message: "heloworld!" });
 })
 
-app.use("/api",apiRouter);
-
+app.use("/api", apiRouter);
 app.use(errorHandle);
+
+app.all("*", (req, res) => {
+    res.status(404).json({ message: "end point does not exist" });
+});
+
 
 //express  port connection code
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`App is listening on port ${port}`)
 })
