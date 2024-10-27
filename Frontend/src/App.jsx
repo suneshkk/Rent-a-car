866baa6c3a128157c233b82b616702d80a165f36
suneshkk <suneshkk232@gmail.com>
2024-10-27 21:05:49 +0530

    change

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from "react-router-dom"
-import { router } from "./routes/routes.jsx"
+import { router } from "./routes/routes"
 import { Toaster } from 'react-hot-toast';
 function App() {
   return (
