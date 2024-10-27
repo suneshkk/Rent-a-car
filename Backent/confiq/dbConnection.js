d82583413269412c29b660efa973a0355db01e62
suneshkk <suneshkk232@gmail.com>
2024-10-27 20:44:01 +0530

    change

--- Backent/confiq/dbConnection.js
+++ Backent/confiq/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.mongoDBURL);
+        await mongoose.connect(process.env.MONGODB_URL);
         console.log("Database connected succesfully")
 
     } catch (error) {
