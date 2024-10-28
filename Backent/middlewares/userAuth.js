e47443f1ec04e9239f886cac0f641a3aa5472795
suneshkk <suneshkk232@gmail.com>
2024-10-28 16:02:57 +0530

    ch

--- Backent/middlewares/userAuth.js
+++ Backent/middlewares/userAuth.js
@@ -4,7 +4,7 @@ export const userAuth = (req, res, next) => {
   try {
     // Extract the token from cookies
     const { token } = req.cookies;
-    // console.log("token",token);
+
     // Check if the token exists
     if (!token) {
       return res.status(401).json({ message: 'User not authorized' });
@@ -12,7 +12,7 @@ export const userAuth = (req, res, next) => {
 
     // Verify the token with the secret key
     const tokenVerified = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    // console.log("token",tokenVerified);
+    
     // If token verification fails, handle it in the catch block
     if (!tokenVerified) {
       return res.status(401).json({ message: 'User not authorized' });
