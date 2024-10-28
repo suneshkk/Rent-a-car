8cc4d114519da6dc3363f9275648aadc30b5b176
suneshkk <suneshkk232@gmail.com>
2024-10-28 11:58:29 +0530

    change

--- Backent/index.js
+++ Backent/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsSetup = {
-    origin: ['https://rent-a-car-tm1f.onrender.com/'],
+    origin: ['https://rent-a-car-1-0ek2.onrender.com/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true // Allow credentials like cookies, authorization headers
