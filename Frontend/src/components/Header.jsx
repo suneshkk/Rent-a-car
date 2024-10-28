dc2cca12612eaca956e57f19529868493a13eafb
suneshkk <suneshkk232@gmail.com>
2024-10-29 00:43:48 +0530

    push

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -28,20 +28,7 @@ function Navbar() {
                 className="text-2xl md:hidden" 
                 onClick={toggleMobileMenu}
             >
-               <svg
-          className="w-6 h-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d={isMobileMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
-          ></path>
-        </svg>
+                {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
             </button>
 
             {/* Links - hidden on mobile by default */}
