5279e50a2956f0bde995e908710ab4d80cd2addd
suneshkk <suneshkk232@gmail.com>
2024-11-23 17:40:10 +0530

    vhagxa

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import HeroImage from "../../src/assets/hero.png";
 import BmwCar from "../../src/assets/bmw.png"
 import Benz from '../../src/assets/benz.png';
 import Car2 from '../../src/assets/car2.png';
-import Car3 from '../../src/assets/car3.png';
+import Mustang from '../../src/assets/mustang.png';
 import { Link } from 'react-router-dom';
 import Loader from '../components/util/Loader.jsx';
 import { FaCar } from "react-icons/fa6";
@@ -50,26 +50,6 @@ function Home() {
             Choose from a wide range of vehicles to suit your needs.
           </p>
         </div>
-        {/* <div className='px-4 sm:px-6 md:px-8 lg:px-12 py-8'>
-          <div className="bg-amber-100 flex h-16 sm:h-20 items-center justify-center mb-4 sm:mb-6">
-            <h1 className="font-extrabold text-xl sm:text-2xl md:text-4xl lg:text-5xl text-center">
-              <Link to={'/car-gallery'}>
-                Book Your car now
-
-              </Link>
-            </h1>
-          </div>
-          {loading ? (<Loader />) : (
-            <div className="grid">
-              <div className='xl:grid xl:grid-cols-4 lg:flex lg:flex-wrap md:grid md:grid-cols-3 sm:flex sm:flex-wrap'>
-                {data.map((value) => (
-                  <CarList car={value} key={value?._id} />
-                ))}
-
-              </div>
-            </div>
-          )}
-        </div> */}
         <div>
           <div className='md:h-28 md:mt-20 '>
             <h1 className='text-slate-400 text-center md:text-6xl font-serif md:font-bold hover:text-slate-100'>The perfect car for your next trip</h1>
@@ -100,7 +80,7 @@ function Home() {
             </div>
           </div>
 
-          <div className='bg-slate-200 bg-cover'>
+          <div className='bg-slate-200 bg-cover pb-6'>
             <div className='md:mx-20 md:pt-20 md:pb-5 md:grid md:grid-cols-3 '>
               <div className='md:h-40 md:pt-3 '>
                 <div className='flex'>
@@ -136,8 +116,8 @@ function Home() {
               </div>
               <div className='md:h-40 md:pt-3 '>
                 <div className='flex'>
-                  <h1 className='text-2xl'>               
-                     <FaCar />
+                  <h1 className='text-2xl'>
+                    <FaCar />
                   </h1>
                   <h1 className='mb-3 font-semibold text-lg underline'>
                     Cars
@@ -150,44 +130,12 @@ function Home() {
               </div>
 
             </div>
+            <div className=' bg-cover bg-center md:mx-40 rounded-md md:h-128'style={{ backgroundImage: `url(${Mustang})` }}>
+            </div>
 
           </div>
 
-          {/* <div className=" flex h-16 sm:h-20 items-center justify-center mb-4 sm:mb-6">
-            <h1 className="text-orange-500 font-extrabold text-xl sm:text-2xl md:text-4xl lg:text-5xl text-center ">
-              Our Future Cars
-            </h1>
-          </div> */}
 
-          <div className="grid gap-6 sm:gap-8 md:gap-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center">
-            {/* <div className="card bg-slate-300 w-72 sm:w-80 md:w-96 shadow-xl">
-              <figure>
-                <img src={Car1} alt="Car" className="w-full h-40 sm:h-48 object-cover" />
-              </figure>
-              <div className="card-body p-4">
-                <h2 className="card-title text-lg md:text-xl">Lamborghini SUV Urus</h2>
-                <p className="text-gray-600 mt-2">Experience luxury and performance.</p>
-              </div>
-            </div> */}
-            {/* <div className="card bg-slate-300 w-72 sm:w-80 md:w-96 shadow-xl">
-              <figure>
-                <img src={Car2} alt="Car" className="w-full h-40 sm:h-48 object-cover" />
-              </figure>
-              <div className="card-body p-4">
-                <h2 className="card-title text-lg md:text-xl">Bentley GT</h2>
-                <p className="text-gray-600 mt-2">Experience luxury and performance.</p>
-              </div>
-            </div> */}
-            {/* <div className="card bg-slate-300 w-72 sm:w-80 md:w-96 shadow-xl">
-              <figure>
-                <img src={Car3} alt="Car" className="w-full h-40 sm:h-48 object-cover" />
-              </figure>
-              <div className="card-body p-4">
-                <h2 className="card-title text-lg md:text-xl">Jaguar Land Rover</h2>
-                <p className="text-gray-600 mt-2">Experience luxury and performance.</p>
-              </div>
-            </div> */}
-          </div>
         </div>
 
       </div>
